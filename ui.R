1c13b1bacc490cb387b0678e680642f182bbebc9
changjung1995 <wqd190008@siswa.um.edu.my>
2019-12-14 18:58:31 +0800

    added xlim and ylim

--- ui.R
+++ ui.R
@@ -16,6 +16,16 @@ vars <-vars[-1:-3]
 
 years<-c("2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012", "2013","2014","2015")
 
+xlim <- list(
+  min = 0,
+  max = 15
+)
+
+ylim <- list(
+  min = 40,
+  max = 100
+)
+
 # Define UI for application that draws a histogram
 navbarPage("Life expectancy", id="nav",
            
